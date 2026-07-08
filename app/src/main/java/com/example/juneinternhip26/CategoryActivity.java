e428fcd9d1d6863c32a4e13ec2c8ee6682cc5428
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2026-07-08 16:06:35 +0530

    Category Adapter

--- app/src/main/java/com/example/juneinternhip26/CategoryActivity.java
+++ app/src/main/java/com/example/juneinternhip26/CategoryActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
@@ -26,8 +27,9 @@ public class CategoryActivity extends AppCompatActivity {
 
         category_recycler = findViewById(R.id.category_recycler);
 
-        category_recycler.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.HORIZONTAL));
-
+//        category_recycler.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.HORIZONTAL));
+        category_recycler.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+//        category_recycler.setLayoutManager(new LinearLayoutManager(CategoryActivity.this));
 
         CategoryAdapter adapter = new CategoryAdapter(CategoryActivity.this, idArray, nameArray, imageArray);
         category_recycler.setAdapter(adapter);
--- app/src/main/java/com/example/juneinternhip26/CategoryAdapter.java
+++ app/src/main/java/com/example/juneinternhip26/CategoryAdapter.java
@@ -1,6 +1,55 @@
 package com.example.juneinternhip26;
 
-public class CategoryAdapter {
-    public CategoryAdapter(CategoryActivity categoryActivity, int[] idArray, String[] nameArray, int[] imageArray) {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyHolder> {
+    Context context;
+    int[] idArray;
+    String[] nameArray;
+    int[] imageArray;
+
+    public CategoryAdapter(Context context, int[] idArray, String[] nameArray, int[] imageArray) {
+        this.context = context;
+        this.idArray = idArray;
+        this.nameArray = nameArray;
+        this.imageArray = imageArray;
+    }
+
+    @NonNull
+    @Override
+    public CategoryAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_category, parent, false);
+        return new MyHolder(view);
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        ImageView category_image;
+        TextView category_name;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            category_image = itemView.findViewById(R.id.category_image);
+            category_name = itemView.findViewById(R.id.category_name);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.MyHolder holder, int position) {
+        holder.category_image.setImageResource(imageArray[position]);
+        holder.category_name.setText(nameArray[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return nameArray.length;
     }
 }
