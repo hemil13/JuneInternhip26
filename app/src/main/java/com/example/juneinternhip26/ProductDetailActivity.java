c0a07c1123e9f9d6a95c21323114767f277f8ce7
Hemil13 <157737404+Hemil13@users.noreply.github.com>
2026-07-27 16:15:21 +0530

    Wishlist toggle product detail page

--- app/src/main/java/com/example/juneinternhip26/ProductDetailActivity.java
+++ app/src/main/java/com/example/juneinternhip26/ProductDetailActivity.java
@@ -2,9 +2,9 @@ package com.example.juneinternhip26;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.TextView;import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,11 +14,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
-    ImageView productImage;
+    ImageView productImage, wishlist;
     TextView productName, productOriginalPrice, productDiscountedPrice, productDescription;
     Button btnAddToCart, btnBuyNow;
 
     SharedPreferences sp;
+
+    Boolean isWishlist = false;
 
 
     @Override
@@ -35,13 +37,35 @@ public class ProductDetailActivity extends AppCompatActivity {
         productDescription = findViewById(R.id.product_description);
         btnAddToCart = findViewById(R.id.btn_add_to_cart);
         btnBuyNow = findViewById(R.id.btn_buy_now);
-
+        wishlist = findViewById(R.id.product_detail_wishlist);
 
         productImage.setImageResource(Integer.parseInt(sp.getString(ConstantSp.productimage, "")));
         productName.setText(sp.getString(ConstantSp.productname, ""));
         productOriginalPrice.setText(ConstantSp.rupees_symbol+sp.getString(ConstantSp.originalprice, ""));
         productDiscountedPrice.setText(ConstantSp.rupees_symbol+sp.getString(ConstantSp.discountprice, ""));
         productDescription.setText(sp.getString(ConstantSp.productdescription, ""));
+
+
+
+
+
+        wishlist.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View view) {
+
+            if(isWishlist){
+                isWishlist = false;
+                wishlist.setImageResource(R.drawable.wishlist_empty);
+                Toast.makeText(ProductDetailActivity.this, "Removed from Wishlist", Toast.LENGTH_SHORT).show();
+            }else{
+                isWishlist = true;
+                wishlist.setImageResource(R.drawable.wishlist_fill);
+                Toast.makeText(ProductDetailActivity.this, "Added to Wishlist", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+            }});
 
 
 
